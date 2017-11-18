1a349653e6e98f46ff5f4544b85439b08e5efcea
Ketki R Andhale <ketki.andhale@gmail.com>
2017-11-18 15:08:41 -0500

    add

--- index.php
+++ index.php
@@ -44,4 +44,18 @@ class collection
       $model = new static::$modelName;
       return $model;
     }
+
+
+    static public function findAll() 
+    {
+        $db = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $recordsSet =  $statement->fetchAll();
+        return $recordsSet;
+    }
 }
