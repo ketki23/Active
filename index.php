1a379d6e7f73cfde35fec5578ab47ea4378bf2c0
Ketki R Andhale <ketki.andhale@gmail.com>
2017-11-18 19:14:51 -0500

    add

--- index.php
+++ index.php
@@ -115,9 +115,25 @@ class model
 class account extends model 
 {
 
+
+
 }
 
 class todo extends model 
 {
     
+    public $id;
+    public $owneremail;
+    public $ownerid;
+    public $createddate;
+    public $duedate;
+    public $message;
+    public $isdone;
+
+    public function __construct()
+    {
+        $this->tableName = 'todos';
+    
+    }
+
 }
